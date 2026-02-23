37fb9f91ee7df614f73ed9628c6751c9279ca177
ronaksharma-simform <ronak.sharma@simformsolutions.com>
2026-02-23 16:45:56 +0530

    restructure : Convert tokenizer , parser and evaluator function into class

--- js/utils/infixToPostfix.js
+++ js/utils/infixToPostfix.js
@@ -1,48 +1,85 @@
 import Stack from "./stack.js";
 import { operators, functions } from "./operations.js";
 
-function getPrecedence(operator) {
-    return operators.has(operator) ?
-        operators.get(operator).precedence :
-        functions.get(operator).precedence;
-}
-export default function infixToPostfix(token) {
-    let output = [];
-    let st = new Stack();
-    for (let key of token) {
-        if (key == "") continue;
-        if (operators.has(key) || functions.has(key)) {
-            while (!st.isEmpty() &&
-                st.peek() !== "(" &&
-                ((operators.has(key) &&
-                        operators.has(st.peek()) &&
-                        ((operators.get(key).associativity === "left" &&
-                                getPrecedence(key) <= getPrecedence(st.peek())) ||
-                            (operators.get(key).associativity === "right" &&
-                                getPrecedence(key) < getPrecedence(st.peek())))) ||
-                    functions.has(st.peek()))
-            ) {
-                output.push(st.pop());
-            }
+export class InfixtoPostfix {
+	constructor() {
+		this.operators = operators;
+		this.functions = functions;
+	}
+	convert(tokens) {
+		if (tokens.length === 0) {
+			throw new Error("Invalid Expression: Empty token list");
+		}
+		const output = [];
+		const st = new Stack();
 
-            st.push(key);
-        } else if (key == "(") {
-            st.push(key);
-        } else if (key == ")") {
-            while (st.peek() != "(") {
-                output.push(st.pop());
-            }
-            st.pop();
-        } else {
-            output.push(key);
-        }
-    }
-    while (!st.isEmpty() && st.peek() !== "(") {
-        output.push(st.pop());
-    }
-    if (!st.isEmpty()) {
-        throw new Error("Invalid Expression")
-    }
-    // console.log(output);
-    return output;
+		for (let token of tokens) {
+			if (token == "") continue;
+			// Handling operator and function
+			if (this.operators.has(token) || this.functions.has(token)) {
+				this.#handleOperator(token, st, output);
+			}
+			// Handling opening parenthesis
+			else if (token == "(") {
+				st.push(token);
+			}
+			// Handling closing parenthesis
+			else if (token == ")") {
+                if(st.isEmpty()){
+                    throw new Error("Invalid Expression : Mismatched parentheses");
+                }
+				while (st.peek() != "(") {
+					output.push(st.pop());
+				}
+				st.pop();
+			}
+			// Handling operands
+			else {
+				output.push(token);
+			}
+		}
+        // pushing remaining operators into expressions
+		while (!st.isEmpty() && st.peek() !== "(") {
+			output.push(st.pop());
+		}
+		if (!st.isEmpty()) {
+			throw new Error("Invalid Expression : Mismatched parentheses");
+		}
+		console.log(output);
+		return output;
+	}
+	#getPrecedence(operator) {
+		return this.operators.has(operator)
+			? this.operators.get(operator).precedence
+			: this.functions.get(operator).precedence;
+	}
+	#handleOperator(token, stack, output) {
+		while (
+			!stack.isEmpty() &&
+			stack.peek() !== "(" &&
+			this.#shouldPop(token, stack.peek())
+		) {
+			output.push(stack.pop());
+		}
+
+		stack.push(token);
+	}
+	#shouldPop(current, top) {
+		if (this.functions.has(top)) return true;
+
+		if (!this.operators.has(current) || !this.operators.has(top)) return false;
+
+		const currentOp = this.operators.get(current);
+		const topOp = this.operators.get(top);
+
+		if (currentOp.associativity === "left") {
+			return currentOp.precedence <= topOp.precedence;
+		}
+
+		if (currentOp.associativity === "right") {
+			return currentOp.precedence < topOp.precedence;
+		}
+
+		return false;
+	}
 }
--- js/utils/postfixEvaluation.js
+++ js/utils/postfixEvaluation.js
@@ -1,21 +1,42 @@
 import Stack from "./stack.js";
 import { operators, functions } from "./operations.js";
-export default function postfixEvaluation(tokens) {
-    let st = new Stack();
-    for (let key of tokens) {
-        if (operators.has(key) || functions.has(key)) {
-            const currentOperands = [];
-            let currentOperator = operators.has(key) ? operators.get(key) : functions.get(key);
-            for (let i = 0; i < currentOperator.arity; i++) {
-                currentOperands.push(parseFloat(st.pop()));
-            }
-            let result = currentOperator.execute(...currentOperands);
+export default class PostfixEvaluator {
+	constructor() {
+		this.operators = operators;
+		this.functions = functions;
+	}
+	evaluate(tokens) {
+		if (!Array.isArray(tokens) || tokens.length === 0) {
+			throw new Error("Invalid postfix expression");
+		}
+		const st = new Stack();
+		for (let token of tokens) {
+			if (this.operators.has(token) || this.functions.has(token)) {
+				const currentOperands = [];
+				// get current operator or function details
+				let currentOperator = this.operators.has(token)
+					? this.operators.get(token)
+					: this.functions.get(token);
 
-            st.push(result);
-        } else {
-            st.push(key);
-        }
-    }
-    console.log(st.peek());
-    return st.peek();
+				if (st.size() < currentOperator.arity) {
+					throw new Error("Invalid Expression: Insufficient operands");
+				}
+
+				for (let i = 0; i < currentOperator.arity; i++) {
+					currentOperands.unshift(parseFloat(st.pop()));
+				}
+				// executing operator on operands
+				let result = currentOperator.execute(...currentOperands);
+
+				st.push(result);
+			} else {
+				st.push(token);
+			}
+		}
+		console.log(st.peek());
+		if (st.size() !== 1) {
+			throw new Error("Invalid Expression: Too many operands");
+		}
+		return st.peek();
+	}
 }
