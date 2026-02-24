bea2483c42bfd226e12e8357132d6e4ad9b31474
ronaksharma-simform <ronak.sharma@simformsolutions.com>
2026-02-24 13:34:46 +0530

    feat : Add methods like setHistory,clearHistory and getHistory in calculator class and integrate with frotend

--- js/utils/calculator.js
+++ js/utils/calculator.js
@@ -11,10 +11,21 @@ export default class Calculator {
 		this.#history = [];
 	}
 	evaluate(expression) {
-		
-		const tokens=this.#tokenizer.tokenParser(expression);
-        const postfix=this.#parser.convert(tokens);
-        const result = this.#evaluator.evaluate(postfix);
-        return result;
+		const tokens = this.#tokenizer.tokenParser(expression);
+		const postfix = this.#parser.convert(tokens);
+		const result = this.#evaluator.evaluate(postfix);
+		return result;
+	}
+	setHistory(expression) {
+		this.#history.push(expression);
+		localStorage.setItem("prev_calculations", this.#history);
+	}
+	clearHistory(expression) {
+		localStorage.clear();
+	}
+	getHistory(expression) {
+		const prev_calculations = localStorage.getItem("prev_calculations");
+		if (prev_calculations) this.#history = prev_calculations.split(",");
+		return this.#history;
 	}
 }
